c905b620aa382e55949af9d1edfd5a6e045d50cf
facss <lzy2lcy@gmail.com>
2021-09-17 17:26:45 +0800

    新增string类的写法

--- 常见的代码模块/my_string.h
+++ 常见的代码模块/my_string.h
@@ -1,3 +1,5 @@
+//简易版本的string，参考陈硕的写法
+
 #pragma once 
 #include <string>
 
@@ -6,9 +8,34 @@ public:
     MyString() : data_(new char[1]) {
         *data_ = '\0';
     }
-    MyString(const char * str) :data_(new char[strlen(str) - 1]) {
-        
+    MyString(const char * str) :data_(new char[strlen(str) + 1]) {
+        strcpy(data_,str);
     }
+    MyString(const string& rhs):data_(new char[rhs.size() + 1]) {
+        strcpy(data_,rhs.c_str());
+    }
+    MyString(strring && rhs) {
+        rhs.data_ = NULL;
+    } 
+    ~MyString() {
+        delete[] data_;
+    }
+
+    MyString& operator= (MyString&& rhs) {
+        std::swap(rhs);
+        return * this;
+    }
+    size_t size() const {
+        return  strlen(data_);
+    }
+
+    const char * c_str() {
+        return data_;
+    }
+    void swap(String& rhs) {
+        std::swap(data_,rhs.data_);
+    }
+
 private:
     char * data_;    
 }
