d9035994e29940fa84df18f6ee86d407510fd1b8
facss <lzy2lcy@gmail.com>
2021-09-17 18:18:32 +0800

    给vector占个坑

--- 常见的代码模块/my_vector.h
+++ 常见的代码模块/my_vector.h
@@ -3,5 +3,22 @@
 #pragma once 
 
 namespace MySTL {
+template <typename T>
+class vector {
+public:
+    typedef T* iterator;
+    typedef T*  reserve_iterator;
+public:
+    vector() : _start(nullptr), _finish(nullptr), _end_of_storage(nullptr) {}
+    vector(size_t n ,const T&value) {
+        _start = new T[n];
+        //向空间中存放n个值为value的元素
 
+    }
+    
+private:
+    T * _start; //实际存储的size开始位置    
+    T * _finish;//实际的存储size末尾位置 
+    T * _end_of_storage;//容量位置，一般来说storage是大于等于finish
+};
 }
